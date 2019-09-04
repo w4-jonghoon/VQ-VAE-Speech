728cc451ed56e6858af6f0b673aceb1db2e2e53b
Jonghoon Seo <jonghoon.seo@wisefour.com>
2019-09-04 18:30:48 +0900

    Flush comments immediatly

--- src/error_handling/color_print.py
+++ src/error_handling/color_print.py
@@ -30,7 +30,7 @@ import sys
 class ColorPrint(object):
     """ Colored printing functions for strings that use universal ANSI escape sequences.
 
-    fail: bold red, pass: bold green, warn: bold yellow, 
+    fail: bold red, pass: bold green, warn: bold yellow,
     info: bold blue, bold: bold white
 
     :source: https://stackoverflow.com/a/47622205
@@ -39,23 +39,29 @@ class ColorPrint(object):
     @staticmethod
     def print_fail(message, end='\n'):
         sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
+        sys.stderr.flush()
 
     @staticmethod
     def print_pass(message, end='\n'):
         sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+        sys.stdout.flush()
 
     @staticmethod
     def print_warn(message, end='\n'):
         sys.stderr.write('\x1b[1;33m' + message.strip() + '\x1b[0m' + end)
+        sys.stdout.flush()
 
     @staticmethod
     def print_info(message, end='\n'):
         sys.stdout.write('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
+        sys.stdout.flush()
 
     @staticmethod
     def print_major_fail(message, end='\n'):
         sys.stdout.write('\x1b[1;35m' + message.strip() + '\x1b[0m' + end)
+        sys.stdout.flush()
 
     @staticmethod
     def print_bold(message, end='\n'):
         sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
+        sys.stdout.flush()
